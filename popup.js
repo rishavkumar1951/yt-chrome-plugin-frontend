6b338ee30c7e830110a303958b93a8fb0d854200
Rishav Kumar <rishavkumar1951@gmail.com>
2025-04-12 00:57:32 +0530

    add v1 of frontend

--- popup.js
+++ popup.js
@@ -2,8 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
     const outputDiv = document.getElementById("output");
-    const API_KEY = 'YOUR_API_KEY';  // Replace with your actual YouTube Data API key
-    const API_URL = 'http://my-elb-2062136355.us-east-1.elb.amazonaws.com:80';
+    const API_KEY = 'AIzaSyCfvYR3qvxivO3qH-r3qfT3qLuqj0Hq8dc';  // Replace with your actual YouTube Data API key
+    const API_URL = 'http://my-elb-2062136355.us-east-1.elb.amazonaws.com:80'; 
   
     // Get the current tab's URL
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
@@ -116,7 +116,7 @@ document.addEventListener("DOMContentLoaded", async () => {
               </ul>
             </div>`;
         }
-      } else {
+    } else {
         outputDiv.innerHTML = "<p>This is not a valid YouTube URL.</p>";
       }
     });
